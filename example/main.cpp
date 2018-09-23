d08df5c6a0c67b86da38ca4237d7092a4eaf442c
Sorin Fetche <sorin.fetche@gmail.com>
2018-09-22 17:25:54 -0700

    Visual Studio warnings

--- example/main.cpp
+++ example/main.cpp
@@ -20,9 +20,9 @@ int main() {
 
     {
         PRINT_ALLOCATIONS_AT_LINE();
-        [[maybe_unused]] auto i1 = std::make_unique<std::uint16_t>(1);
+        [[maybe_unused]] auto i1 = std::make_unique<std::uint16_t>(std::uint16_t(0));
         PRINT_ALLOCATIONS_AT_LINE();
-        [[maybe_unused]] auto i2 = std::make_unique<std::uint8_t>(2);
+        [[maybe_unused]] auto i2 = std::make_unique<std::uint8_t>(std::uint8_t(0));
         PRINT_ALLOCATIONS_AT_LINE();
         // 3rd allocation (after we allocated 3 bytes)
         try {
@@ -38,12 +38,12 @@ int main() {
     PRINT_ALLOCATIONS_AT_LINE();
 
     // Re-allocating 3 bytes.
-    [[maybe_unused]] auto i1 = std::make_unique<std::uint8_t>(1);
-    [[maybe_unused]] auto i2 = std::make_unique<std::uint8_t>(1);
-    [[maybe_unused]] auto i3 = std::make_unique<std::uint8_t>(1);
+    [[maybe_unused]] auto i1 = std::make_unique<std::uint8_t>(std::uint8_t(0));
+    [[maybe_unused]] auto i2 = std::make_unique<std::uint8_t>(std::uint8_t(0));
+    [[maybe_unused]] auto i3 = std::make_unique<std::uint8_t>(std::uint8_t(0));
     try {
         // This should fail again as we reach the amount of allocated memory at the time of the first generated error.
-        [[maybe_unused]] auto c = std::make_unique<char>(0);
+        [[maybe_unused]] auto c = std::make_unique<char>('a');
         std::cerr << "Error: We shouldn't get here !" << std::endl;
     } catch (std::exception &e) {
         dev_new::pause_error_testing();
