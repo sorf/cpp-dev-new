ca5fbf5d19c8a81d9cb1e3bc2e830fe7cbddde9f
Sorin <sorin.fetche@gmail.com>
2018-10-07 10:40:39 -0700

    test_package/conanfile: Increase warning level

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,6 +10,13 @@ class DevNewExampleConan(ConanFile):
         cmake = CMake(self)
         cmake.verbose = True
 
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["CONAN_CXX_FLAGS"] += " /W4 /WX"
+        elif self.settings.compiler == "gcc":
+            cmake.definitions["CONAN_CXX_FLAGS"] += " -Wall -Wextra -Werror"
+        elif self.settings.compiler == "clang":
+            cmake.definitions["CONAN_CXX_FLAGS"] += " -Wall -Wextra -Werror -Wglobal-constructors"
+
         if self.options["dev_new"].clang_tidy:
             # If testing with clang_tidy, removing the CONAN_LIBCXX flag so that we do not get a
             # clang-tidy warning/error from _GLIBCXX_USE_CXX11_ABI being defined
