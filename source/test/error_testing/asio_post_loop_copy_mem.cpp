d545b6b094c26328b95e40f62779a53d56b0fdb9
Sorin <sorin.fetche@gmail.com>
2018-11-07 22:41:55 -0800

    The buffer type named as buffer_t

--- source/test/error_testing/asio_post_loop_copy_mem.cpp
+++ source/test/error_testing/asio_post_loop_copy_mem.cpp
@@ -7,10 +7,10 @@
 
 namespace {
 
-using buffer = std::vector<char>;
+using buffer_t = std::vector<char>;
 
 // NOLINTNEXTLINE(performance-unnecessary-value-param)
-void posted_func(asio::io_context &io_context, unsigned count, buffer buf) {
+void posted_func(asio::io_context &io_context, unsigned count, buffer_t buf) {
     dev_new::run_no_error_testing([&] { std::cout << "posted_func: " << count << std::endl; });
     if (count != 0) {
         io_context.post([&io_context, count, buf] { posted_func(io_context, count - 1, buf); });
@@ -22,7 +22,7 @@ void posted_func(asio::io_context &io_context, unsigned count, buffer buf) {
 int main() {
     error_testing::run_loop([] {
         asio::io_context io_context;
-        io_context.post([&io_context, count = 3, buf = buffer(10)] { posted_func(io_context, count, buf); });
+        io_context.post([&io_context, count = 3, buf = buffer_t(10)] { posted_func(io_context, count, buf); });
         io_context.run();
     });
     return 0;
--- source/test/error_testing/asio_post_loop_move_mem.cpp
+++ source/test/error_testing/asio_post_loop_move_mem.cpp
@@ -7,9 +7,9 @@
 
 namespace {
 
-using buffer = std::vector<char>;
+using buffer_t = std::vector<char>;
 
-void posted_func(asio::io_context &io_context, unsigned count, buffer buf) {
+void posted_func(asio::io_context &io_context, unsigned count, buffer_t buf) {
     dev_new::run_no_error_testing([&] { std::cout << "posted_func: " << count << std::endl; });
     if (count != 0) {
         io_context.post([&io_context, count, buf = std::move(buf)]() mutable {
@@ -24,7 +24,7 @@ int main() {
     error_testing::run_loop([] {
         asio::io_context io_context;
         io_context.post(
-            [&io_context, count = 3, buf = buffer(10)]() mutable { posted_func(io_context, count, std::move(buf)); });
+            [&io_context, count = 3, buf = buffer_t(10)]() mutable { posted_func(io_context, count, std::move(buf)); });
         io_context.run();
     });
     return 0;
