55facb167c701b61bbcceec55039d3a40fd9d433
Sorin <sorin.fetche@gmail.com>
2018-11-19 23:31:34 -0800

    asio_post_loop_move_mem_v2

---
+++ source/test/error_testing/asio_post_loop_move_mem_v2.cpp
@@ -0,0 +1,31 @@
+// Error testing of a loop (chain) of asio::io_context::post() calls with memory moving at each call.
+#include "dev_new.hpp"
+#include "run_loop.hpp"
+
+#include <asio/io_context.hpp>
+#include <asio/post.hpp>
+#include <vector>
+
+namespace {
+
+using buffer_t = std::vector<char>;
+
+void do_post(asio::io_context &io_context, unsigned count, buffer_t buffer) {
+    dev_new::run_no_error_testing([&] { std::cout << "do_post: " << count << std::endl; });
+    if (count != 0) {
+        asio::post(io_context, [&io_context, count, buffer = std::move(buffer)]() mutable {
+            do_post(io_context, count - 1, std::move(buffer));
+        });
+    }
+}
+
+} // namespace
+
+int main() {
+    error_testing::run_loop([] {
+        asio::io_context io_context;
+        do_post(io_context, 3, buffer_t(10));
+        io_context.run();
+    });
+    return 0;
+}
