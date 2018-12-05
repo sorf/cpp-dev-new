7cc341450b28fdec9aed5483f2eb2c430949b04b
Sorin Fetche <sorin.fetche@gmail.com>
2018-12-05 04:01:56 -0800

    Set _WIN32_WINNT as Win10 and ASIO_DISABLE_BOOST_DATE_TIME

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,9 @@ class DevNewConan(ConanFile):
         cmake = CMake(self)
         cmake.verbose = True
         if self.settings.compiler == "Visual Studio":
-            cmake.definitions["CONAN_CXX_FLAGS"] += " /W4 /WX /D_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS"
+            cmake.definitions["CONAN_CXX_FLAGS"] += " /W4 /WX /D_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS" +\
+                                                    " /D_WIN32_WINNT=0x0A00" +\
+                                                    " /DASIO_DISABLE_BOOST_DATE_TIME"
         elif self.settings.compiler == "gcc":
             cmake.definitions["CONAN_CXX_FLAGS"] += " -Wall -Wextra -Werror"
         elif self.settings.compiler == "clang":
