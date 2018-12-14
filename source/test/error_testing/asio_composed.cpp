8b770fc0f153496d66689628e9170d9fb9116ac3
Sorin <sorin.fetche@gmail.com>
2018-12-14 06:43:58 -0800

    asio_composed.cpp: Re-implement the composed operation to use many timers running both in parallel and in series. (work in progress)

--- source/test/error_testing/asio_composed.cpp
+++ source/test/error_testing/asio_composed.cpp
@@ -1,162 +1,139 @@
 // Error testing of the Asio composed operation example.
-// See: https://github.com/chriskohlhoff/asio/blob/master/asio/src/examples/cpp11/operations/composed_5.cpp
-// for detailed comments.
+// Based on https://github.com/chriskohlhoff/asio/blob/master/asio/src/examples/cpp11/operations/composed_5.cpp
 #include "dev_new.hpp"
 #include "run_loop.hpp"
 
-//
-// composed_5.cpp
-// ~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-#include <asio/executor_work_guard.hpp>
+#include <asio/bind_executor.hpp>
 #include <asio/io_context.hpp>
-#include <asio/ip/tcp.hpp>
 #include <asio/steady_timer.hpp>
 #include <asio/use_future.hpp>
-#include <asio/write.hpp>
-#include <functional>
+#include <boost/format.hpp>
+#include <boost/range/irange.hpp>
+#include <chrono>
 #include <iostream>
 #include <memory>
-#include <sstream>
-#include <string>
-#include <type_traits>
-#include <utility>
+#include <random>
 
-using asio::ip::tcp;
-
-// Composed operation.
-template <typename T, typename CompletionToken>
-auto async_write_messages(tcp::socket &socket, T const &message, std::size_t repeat_count, CompletionToken &&token)
-    // The return type of the initiating function is deduced from the CompletionToken type and the completion handler.
-    -> typename asio::async_result<typename std::decay<CompletionToken>::type, void(std::error_code)>::return_type {
+template <typename Executor, typename CompletionToken>
+auto async_many_timers(Executor &&executor, std::chrono::steady_clock::duration run_duration, CompletionToken &&token)
+    -> typename asio::async_result<std::decay_t<CompletionToken>, void(std::error_code)>::return_type {
 
     using completion_handler_sig = void(std::error_code);
     using completion_type = asio::async_completion<CompletionToken, completion_handler_sig>;
     using completion_handler_type = typename completion_type::completion_handler_type;
 
-    // The intermediate completion handler.
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init, hicpp-member-init)
-    struct intermediate_completion_handler {
-
-        tcp::socket &m_socket;
-        std::unique_ptr<std::string> m_encoded_message;
-        std::size_t m_repeat_count;
-        std::unique_ptr<asio::steady_timer> m_delay_timer;
-        enum { starting, waiting, writing } m_state;
-        asio::executor_work_guard<tcp::socket::executor_type> m_io_work;
-        completion_handler_type m_user_handler;
-
-        void operator()(const std::error_code &error, std::size_t /*unused*/ = 0) {
-            if (!error) {
-                switch (m_state) {
-                case starting:
-                case writing:
-                    if (m_repeat_count > 0) {
-                        --m_repeat_count;
-                        m_state = waiting;
-                        m_delay_timer->expires_after(std::chrono::seconds(1));
-                        m_delay_timer->async_wait(std::move(*this));
-                        return; // Composed operation not yet complete.
-                    }
-                    break; // Composed operation complete, continue below.
-                case waiting:
-                    m_state = writing;
-                    asio::async_write(m_socket, asio::buffer(*m_encoded_message), std::move(*this));
-                    return; // Composed operation not yet complete.
-                }
+    struct internal_state : public std::enable_shared_from_this<internal_state> {
+        internal_state(Executor &&executor_, completion_handler_type user_completion_handler_)
+            : executor(std::move(executor_)),
+              user_completion_handler{std::move(user_completion_handler_)}, run_timer{executor.context()},
+              internal_timers(25), is_open(false) {
+            for (auto &timer : internal_timers) {
+                timer.reset(new asio::steady_timer{executor.context()});
             }
-
-            // We get here only on completion of the entire composed operation.
-            m_io_work.reset();         // no future work for the I/O executor.
-            m_encoded_message.reset(); // deallocate the encoded message
-            m_user_handler(error);     // Call the user-supplied handler with the result of the operation.
         }
 
-        // For correctness we must preserve the executor of the user-supplied completion handler.
-        using executor_type = asio::associated_executor_t<completion_handler_type, tcp::socket::executor_type>;
+        void start_many_waits(std::chrono::steady_clock::duration run_duration) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> one_wait_distribution(1, internal_timers.size());
+
+            // Setting the total run duration.
+            run_timer.expires_after(run_duration);
+            run_timer.async_wait(
+                asio::bind_executor(get_executor(), [this, self = this->shared_from_this()](asio::error_code ec) {
+                    close();
+                    // TODO: This should be called after all the internal timers have completed their cancelling
+                    // (no just initiated)
+                    user_completion_handler(ec);
+                }));
+
+            // Starting the internal timers in parallel.
+            for (auto timer_index : boost::irange<std::size_t>(0, internal_timers.size())) {
+                start_one_wait(timer_index, std::chrono::milliseconds(one_wait_distribution(gen)));
+            }
+            dev_new::run_no_error_testing([&] { std::cout << "timers started" << std::endl; });
+            is_open = true;
+        }
+
+        void close() {
+            run_timer.cancel();
+            for (auto &timer : internal_timers) {
+                timer->cancel();
+            }
+            dev_new::run_no_error_testing([&] { std::cout << "timers cancelled" << std::endl; });
+        }
+
+        void start_one_wait(std::size_t timer_index, std::chrono::steady_clock::duration one_wait) {
+            auto &timer = internal_timers[timer_index];
+            timer->expires_after(one_wait);
+            timer->async_wait(
+                asio::bind_executor(get_executor(), [this, timer_index, one_wait = std::move(one_wait),
+                                                     self = this->shared_from_this()](asio::error_code ec) {
+                    if (is_open) {
+                        start_one_wait(timer_index, std::move(one_wait));
+                    } else if (!is_open) {
+                        dev_new::run_no_error_testing(
+                            [&] { std::cout << boost::format("timer[%d]: wait closed") % timer_index << std::endl; });
+                    } else if (ec == asio::error::operation_aborted) {
+                        dev_new::run_no_error_testing([&] {
+                            std::cout << boost::format("timer[%d]: wait cancelled") % timer_index << std::endl;
+                        });
+                    } else {
+                        dev_new::run_no_error_testing([&] {
+                            std::cout << boost::format("timer[%d]: wait error: %s:%s") % ec % ec.message() << std::endl;
+                        });
+                        close();
+                    }
+                }));
+        }
+
+        using executor_type = asio::associated_executor_t<completion_handler_type, Executor>;
         executor_type get_executor() const noexcept {
-            return asio::get_associated_executor(m_user_handler, m_socket.get_executor());
+            return asio::get_associated_executor(user_completion_handler, executor);
         }
 
-        // Although not necessary for correctness, we preserve the allocator of the user-supplied completion handler.
-        using allocator_type = asio::associated_allocator_t<completion_handler_type, std::allocator<void>>;
-        allocator_type get_allocator() const noexcept {
-            return asio::get_associated_allocator(m_user_handler, std::allocator<void>{});
-        }
+        Executor executor;
+        completion_handler_type user_completion_handler;
+        asio::steady_timer run_timer;
+        std::vector<std::unique_ptr<asio::steady_timer>> internal_timers;
+        bool is_open;
     };
 
     completion_type completion(token);
-
-    std::ostringstream os;
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay, hicpp-no-array-decay)
-    os << message;
-    std::unique_ptr<std::string> encoded_message(new std::string(os.str()));
-
-    std::unique_ptr<asio::steady_timer> delay_timer(new asio::steady_timer(socket.get_executor().context()));
-
-    // Initiate the underlying operations by calling the intermediate completion handler's function call operator.
-    intermediate_completion_handler{socket,
-                                    std::move(encoded_message),
-                                    repeat_count,
-                                    std::move(delay_timer),
-                                    intermediate_completion_handler::starting,
-                                    asio::make_work_guard(socket.get_executor()),
-                                    std::move(completion.completion_handler)}({});
-
+    std::make_shared<internal_state>(std::move(executor), std::move(completion.completion_handler))
+        ->start_many_waits(run_duration);
     return completion.result.get();
 }
 
-// Test our asynchronous operation using a lambda as a callback.
-void test_callback() {
+void test_callback(std::chrono::steady_clock::duration run_duration) {
     asio::io_context io_context;
-#ifndef __clang_analyzer__
-    tcp::acceptor acceptor(io_context, {tcp::v4(), 55555});
-    tcp::socket socket = acceptor.accept();
-#else
-    tcp::socket socket(io_context);
-#endif
-    async_write_messages(socket, "Testing callback\r\n", 5, [](const std::error_code &error) {
-        if (!error) {
-            std::cout << "Messages sent\n";
+    async_many_timers(io_context.get_executor(), run_duration, [](std::error_code const &error) {
+        if (error) {
+            std::cout << "[callback] Timers error: " << error.message() << std::endl;
         } else {
-            std::cout << "Error: " << error.message() << "\n";
+            std::cout << "[callback] Timers done" << std::endl;
         }
     });
-
     io_context.run();
 }
 
-// Test our asynchronous operation using the use_future completion token.
-void test_future() {
+void test_future(std::chrono::steady_clock::duration run_duration) {
     asio::io_context io_context;
-#ifndef __clang_analyzer__
-    tcp::acceptor acceptor(io_context, {tcp::v4(), 55555});
-    tcp::socket socket = acceptor.accept();
-#else
-    tcp::socket socket(io_context);
-#endif
-    std::future<void> f = async_write_messages(socket, "Testing future\r\n", 5, asio::use_future);
+    std::future<void> f = async_many_timers(io_context.get_executor(), run_duration, asio::use_future);
     io_context.run();
 
     try {
-        // Get the result of the operation.
         f.get();
-        std::cout << "Messages sent\n";
+        std::cout << "[future] Timers done" << std::endl;
     } catch (std::exception const &e) {
-        std::cout << "Error: " << e.what() << "\n";
+        std::cout << "[future] Timers error: " << e.what() << std::endl;
     }
-
 }
 
 int main() {
     try {
-        test_callback();
-        test_future();
+        test_callback(std::chrono::seconds(3));
+        test_future(std::chrono::seconds(3));
     } catch (std::exception const &e) {
         std::cout << "Error: " << e.what() << "\n";
     }
